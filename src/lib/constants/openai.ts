a80ea60d179a8b3867bd74904c5c24bd99006a92
Nico Krätschmer <n.kraetschmer@graustich.com>
2023-08-31 11:36:50 +0200

    Add 16k model

--- src/lib/constants/openai.ts
+++ src/lib/constants/openai.ts
@@ -1,5 +1,6 @@
 export const CHATGPT_MODELS = Object.freeze({
   "gpt-3.5-turbo": { label: "GPT-3.5 turbo", tokens: 4096 },
+  "gpt-3.5-turbo-16k": { label: "GPT-3.5 turbo 16k", tokens: 16384 },
   "gpt-3.5-turbo-0301": { label: "GPT-3.5 turbo 0301", tokens: 4096 },
   "gpt-4": { label: "GPT-4", tokens: 8192 },
   "gpt-4-0314": { label: "GPT-4 0314", tokens: 8192 },
